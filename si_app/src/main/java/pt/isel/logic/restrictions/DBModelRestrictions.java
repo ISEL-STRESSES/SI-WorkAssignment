2382bbad538b2d633959adfd70442ec2f15f4aaf
Carlos Guilherme Pereira <74246462+Sideghost@users.noreply.github.com>
2023-05-19 01:22:02 +0100

    beginning of DB and entities restrictions

---
+++ si_app/src/main/java/pt/isel/logic/restrictions/DBModelRestrictions.java
@@ -0,0 +1,19 @@
+package pt.isel.logic.restrictions;
+
+import pt.isel.model.entities.player.Player;
+
+public class DBModelRestrictions{
+
+    public static class PlayerRestrictions implements Restrictions<Player> {
+        @Override
+        public Boolean checkDBRestrictions(Player entity) throws RestrictionException {
+            if (!entity.getEmail().isValid())
+                throw new RestrictionException("Email is not valid");
+
+            if (!entity.getEstado().matches("^(ativo|banido|inativo)$"))
+                throw new RestrictionException("Estado is not valid");
+
+            return true;
+        }
+    }
+}
---
+++ si_app/src/main/java/pt/isel/logic/restrictions/PlayerRestrictions.java
@@ -0,0 +1,6 @@
+package pt.isel.logic.restrictions;
+
+import pt.isel.model.entities.player.Player;
+
+public interface PlayerRestrictions extends Restrictions<Player> {
+}
