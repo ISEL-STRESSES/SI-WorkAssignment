6ce8210f493d71666087af9ac2c30602b7b9f7a0
Carlos Guilherme Pereira <74246462+Sideghost@users.noreply.github.com>
2023-05-19 01:29:05 +0100

    Region Mapper Missing

---
+++ si_app/src/main/java/pt/isel/logic/mappers/region/RegionMapper.java
@@ -0,0 +1,7 @@
+package pt.isel.logic.mappers.region;
+
+import pt.isel.logic.mappers.DataMapper;
+import pt.isel.model.entities.region.Region;
+
+public interface RegionMapper extends DataMapper<Region, String> {
+}
