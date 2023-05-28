ca76e31c256e1b1f809ea380c4142e83f26283ac
Carlos Guilherme Pereira <74246462+Sideghost@users.noreply.github.com>
2023-05-28 15:45:53 +0100

    Basic test for checking repo status

---
+++ si_app/src/test/java/pt/isel/ReposTest.java
@@ -0,0 +1,17 @@
+package pt.isel;
+
+import org.junit.Test;
+import pt.isel.dataAccess.JPAContext;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ReposTest {
+    private final JPAContext ctx = new JPAContext();
+
+    @Test
+    public void PlayerRepoTest() {
+        ctx.connect();
+        assertNotNull(ctx.getPlayers().findAll());
+        ctx.close();
+    }
+}
